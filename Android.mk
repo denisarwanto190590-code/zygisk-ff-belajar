515c310da247373f23a4442964ea09ecef66cd62
denisarwanto190590-code <denisarwanto190590@gmail.com>
2026-07-17 14:30:35 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,19 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# Nama modul disamakan persis dengan id di module.prop agar tidak error
 LOCAL_MODULE := zygisk_ff_belajar
-
-# Mendaftarkan file kode C++ utama
 LOCAL_SRC_FILES := main.cpp
 
-# Menambahkan pustaka sistem Android agar fungsi LOGI (logcat) bisa bekerja
 LOCAL_LDLIBS := -llog -landroid
 
-# PERBAIKAN: Mengaktifkan C++20 khusus untuk C++ agar zygisk.h tidak error makro
+# WAJIB MENGGUNAKAN CPPFLAGS DAN C++20 UNTUK MENGHILANGKAN ERROR MAKRO ZYGISK
 LOCAL_CPPFLAGS := -std=c++20
-
-# Standar keamanan dan optimasi tambahan untuk compiler
 LOCAL_CFLAGS := -Wall -O3
 
 include $(BUILD_SHARED_LIBRARY)
