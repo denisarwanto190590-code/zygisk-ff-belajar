f3de99f2763edf83be836470c8c09b37c1ad1ef6
denisarwanto190590-code <denisarwanto190590@gmail.com>
2026-07-17 14:57:57 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,10 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+# Nama modul disamakan persis dengan id di module.prop agar tidak error
 LOCAL_MODULE := zygisk_ff_belajar
 LOCAL_SRC_FILES := main.cpp
 
-LOCAL_LDLIBS := -llog -landroid
+# PERBAIKAN: Ditambahkan -lGLESv2 agar fungsi grafis glLineWidth dari imgui_overlay.h dikenali
+LOCAL_LDLIBS := -llog -landroid -lGLESv2
 
 # WAJIB MENGGUNAKAN CPPFLAGS DAN C++20 UNTUK MENGHILANGKAN ERROR MAKRO ZYGISK
 LOCAL_CPPFLAGS := -std=c++20
